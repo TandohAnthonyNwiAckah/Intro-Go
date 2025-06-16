2c7f4dbd0a83bacd6b7b912c503adacf62acc359
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-06-16 13:59:01 +0000

    commit loops

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
+import "fmt"
 
 func main() {
 
@@ -100,26 +96,57 @@ func main() {
 	// rangeOne = append(rangeOne, "yaba")
 	// fmt.Println(rangeOne)
 
-	greeting := "hello there friends!"
+	// greeting := "hello there friends!"
 
-	fmt.Println(strings.Contains(greeting, "there")) // true
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "fr"))
-	fmt.Println(strings.Split(greeting, " "))
+	// fmt.Println(strings.Contains(greeting, "there")) // true
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "fr"))
+	// fmt.Println(strings.Split(greeting, " "))
 
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-	sort.Ints(ages)
-	fmt.Println(ages)
+	// ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	// sort.Ints(ages)
+	// fmt.Println(ages)
 
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
+	// index := sort.SearchInts(ages, 30)
+	// fmt.Println(index)
 
-	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+	// names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
 
-	sort.Strings(names)
+	// sort.Strings(names)
+	// fmt.Println(names)
+
+	// fmt.Println(sort.SearchStrings(names, "bowser"))
+
+	// Loops
+
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++ // infinite loop without this
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value of i is:", i)
+	// }
+
+	names := []string{"tony", "jane", "joe", "mary"}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	// for index, val := range names {
+	// 	fmt.Printf("the value at pos %v is %v \n", index, val)
+	// 	val = ""
+	// }
+
+	for _, val := range names {
+		fmt.Print(val, ",")
+		val = "new string"
+	}
+
+	// changing val in a loop does not mutate the original slice
 	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "bowser"))
 
 }

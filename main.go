07c199ee27c6aea231bb154cb0e74334abb801d8
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-06-16 14:09:22 +0000

    commit Booleans & Condoitionals

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func main() {
 	// 	fmt.Println("value of i is:", i)
 	// }
 
-	names := []string{"tony", "jane", "joe", "mary"}
+	// names := []string{"tony", "jane", "joe", "mary"}
 
 	// for i := 0; i < len(names); i++ {
 	// 	fmt.Println(names[i])
@@ -141,12 +141,43 @@ func main() {
 	// 	val = ""
 	// }
 
-	for _, val := range names {
-		fmt.Print(val, ",")
-		val = "new string"
+	// for _, val := range names {
+	// 	fmt.Print(val, ",")
+	// 	val = "new string"
+	// }
+
+	// // changing val in a loop does not mutate the original slice
+	// fmt.Println(names)
+
+	//  booleans & conditionals
+
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
 
-	// changing val in a loop does not mutate the original slice
-	fmt.Println(names)
+	names := []string{"tony", "mary", "joe", "doe", "jane"}
+
+	for index, val := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, val)
+	}
 
 }

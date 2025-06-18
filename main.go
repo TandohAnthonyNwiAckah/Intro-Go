17717c9b574839c6ca2454b4ed2558774824c2d1
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-06-18 14:19:22 +0000

    commit Receiver function

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"hello/hello"
-)
+import "fmt"
 
 var score = 99.5
 
 func main() {
+
 	// fmt.Println("Hello, World!")
 
 	// Print
 	// tutPrint()
 
-	result := hello.SayHello("Tony")
-	fmt.Println(result)
+	// Export hello package
+	// result := hello.SayHello("Tony")
+	// fmt.Println(result)
 
 	// Arrays & Slices
 	// tutArray()
@@ -34,5 +33,19 @@ func main() {
 
 	// Pointers
 	// tutPointer()
+
+	// Structs
+
+	mybill := newBill("Tony bill")
+
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 8.95)
+	mybill.addItem("toffee pudding", 4.95)
+	mybill.addItem("coffee", 3.25)
+
+	mybill.updateTip(10)
+
+	// fmt.Println("Bill Name:", mybill)
+	fmt.Println(mybill.format())
 
 }
